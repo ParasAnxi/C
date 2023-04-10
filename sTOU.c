f112328d3706a1032b982cc41d35d2c5cb7a9095
ParasAnxi <paras.bhandari1010@gmail.com>
2023-04-10 11:32:29 +0530

    19

---
+++ sTOU.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<ctype.h>
+int main(){
+	int ctr=0;
+	char str_char;
+	char str[100];    
+    printf("\n Input a string in lowercase : ");
+    fgets(str, sizeof str, stdin);  
+    printf("string in UPPERCASE :\n ");
+	while (str[ctr]){
+		str_char=str[ctr];
+		putchar (toupper(str_char));
+		ctr++;
+	}
+	printf("\n\n");
+	return 0;
+}
